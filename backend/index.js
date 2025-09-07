39ce88e58d4c4410805647da5e4f2233dd899a49
Sachin7225 <126483766+Sachin7225@users.noreply.github.com>
2025-09-07 20:35:31 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["http://localhost:5173" , "http://localhost:5174"],
+ origin:["https://shopfusion-5-frontend10.onrender.com" , "http://localhost:5174"],
  credentials:true
 }))
 
